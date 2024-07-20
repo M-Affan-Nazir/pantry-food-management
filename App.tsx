8d25f1f393eb5abbb151fd429e2c922e63de1fcb
M-Affan-Nazir <m.affann80@hotmail.com>
2024-07-20 13:27:25 -0600

    s

--- App.tsx
+++ App.tsx
@@ -21,7 +21,7 @@ function MyStack(){
 export default function App() {
   return(
     <NavigationContainer>
-      <StatusBar backgroundColor="lightgreen" barStyle="light-content" />
+      <StatusBar backgroundColor="green" barStyle="light-content" />
       <MyStack />
     </NavigationContainer>
   )  
