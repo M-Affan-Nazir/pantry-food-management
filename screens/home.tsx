dd00b0a74de9ce35d3fcde7ad085b4a8deaff007
M-Affan-Nazir <m.affann80@hotmail.com>
2024-07-24 14:16:28 -0600

    Encapsulating FlatList About-to-Expire and Expired

--- screens/home.tsx
+++ screens/home.tsx
@@ -41,6 +41,7 @@ export default function Home(x : NavigationContainerProp) {
   const [modalOptionVisible, setModalOptionVisible] = useState(false);
   const [db, setDb] = useState<SQLiteDatabase|null>(null)
   const [items, setItems] = useState<itemTypeOut[]>([])
+  const [expired, setExpired] = useState<itemTypeOut[]>([])
 
   useEffect(()=>{
     async function loadDb(){
@@ -83,7 +84,7 @@ export default function Home(x : NavigationContainerProp) {
     { key: '5', name: 'Turkey', days: '4 days' },
   ];
 
-  let renderItem = ({ item }: { item: Item }) => (
+  let renderItem = ({ item }: { item: itemTypeOut }) => (
     <View
       style={{
         backgroundColor: 'white',
@@ -98,10 +99,10 @@ export default function Home(x : NavigationContainerProp) {
         marginRight: 10, // Add some margin for spacing between items
       }}
     >
-      <Text style={{ color: 'black', fontSize: 21, fontWeight: 'bold' }}>
+      <Text style={{ color: 'black', fontSize: 17, fontWeight: 'bold' }}>
         {item.name}
       </Text>
-      <Text style={{ fontSize: 11,  color:"black",  }}>{item.days}</Text>
+      <Text style={{ fontSize: 11,  color:"black",  }}>{item.expiry} {item.factor}</Text>
     </View>
   );
 
@@ -126,6 +127,52 @@ export default function Home(x : NavigationContainerProp) {
     </View>
   );
 
+
+  function AboutToExpireCapsulation(){
+    if(items.length > 0){
+      return (<FlatList
+          horizontal
+          data={items}
+          renderItem={renderItem}
+        
+          keyExtractor={(item) => String(item.id)}
+          showsHorizontalScrollIndicator={false}
+        />)
+    }
+    else{
+      return(
+
+        <View style={{height: 95, width: 95, borderColor:"grey", borderStyle:"dashed", borderWidth:1.3, justifyContent:"center", alignItems:"center", marginTop: 17, marginRight: 10,}} >
+            <Text>No Items</Text>
+        </View>
+
+      )
+    }
+  }
+
+
+  function ExpiredCapsulation(){
+    if(expired.length > 0){
+      return (
+      <FlatList
+        horizontal
+        data={data}
+        renderItem={renderExpired}
+        keyExtractor={(item) => item.key}
+        showsHorizontalScrollIndicator={false}
+      />)
+    }
+    else{
+      return(
+
+        <View style={{height: 55, width: 95, borderColor:"grey", borderStyle:"dashed", borderWidth:1.3, justifyContent:"center", alignItems:"center", marginTop: 17, marginRight: 10,}} >
+            <Text>No Items</Text>
+        </View>
+
+      )
+    }
+  }
+
   return (
     <View style={styles.container}>
       <TouchableOpacity style= {{backgroundColor:"white", padding:5, borderRadius:100, top:h-125, left:w/1.21, position:"absolute", borderColor:"#17bd24", borderWidth:2.1, justifyContent:"center", alignItems:"center"}} onPress={toggleOptionModal} >
@@ -137,27 +184,14 @@ export default function Home(x : NavigationContainerProp) {
           <Ionicons name="timer-outline" size={17} style={{marginTop:1}} color="black" />
         </View>
         <Text style={{position:"absolute", marginTop:16.5, fontSize:13,  color:"black",  marginLeft: 19, fontWeight:"500"}}> Products About to Expire: </Text>
-        <FlatList
-          horizontal
-          data={data}
-          renderItem={renderItem}
-        
-          keyExtractor={(item) => item.key}
-          showsHorizontalScrollIndicator={false}
-        />
+        <AboutToExpireCapsulation />
       </View>
       <View style={{backgroundColor:"white", paddingHorizontal:25,paddingVertical:25, borderRadius:7, marginHorizontal:w/33, marginTop:17, borderWidth:0.5, borderColor:"red"}}>
         <View style={{height:25, width:20, backgroundColor:"red", position:"absolute", marginTop:13, borderTopRightRadius:15, borderBottomRightRadius:15, justifyContent:"center",alignItems:"center"}}>
           <Entypo name="cross" size={17} style={{marginTop:1}} color="white"/>
         </View>
         <Text style={{position:"absolute",  color:"black",  marginTop:16.5, fontSize:13, marginLeft: 19, fontWeight:"500"}}> Expired: </Text>
-        <FlatList
-          horizontal
-          data={data}
-          renderItem={renderExpired}
-          keyExtractor={(item) => item.key}
-          showsHorizontalScrollIndicator={false}
-        />
+        <ExpiredCapsulation />
       </View>
       <Stat />
       <Modal visible={modalOptionVisible} animationType="none" onRequestClose={toggleOptionModal} transparent={true} >
